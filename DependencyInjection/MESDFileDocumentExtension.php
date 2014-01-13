5ada76d2c1a6a759047bf72af0fabb101e539fd1
lighthart <dlighthart@mesd.k12.or.us>
2014-01-13 10:49:34 -0800

    Updated comments

--- DependencyInjection/MESDFileDocumentExtension.php
+++ DependencyInjection/MESDFileDocumentExtension.php
@@ -7,15 +7,13 @@ use Symfony\Component\Config\FileLocator;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader;
 
-/**
- * This is the class that loads and manages your bundle configuration
- *
- * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
- */
 class MESDFileDocumentExtension extends Extension
 {
     /**
-     * {@inheritDoc}
+     * Build the extension services
+     *
+     * @param array $configs
+     * @param ContainerBuilder $container
      */
     public function load( array $configs, ContainerBuilder $container ) {
         $configuration = new Configuration();
