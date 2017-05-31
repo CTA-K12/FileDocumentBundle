2f5f3ae5da9b93c8e74965668c355f6357133857
lighthart <dlighthart@mesd.k12.or.us>
2017-05-31 09:19:55 -0700

    removing deprecated bindRequest in controller

--- Controller/DocumentController.php
+++ Controller/DocumentController.php
@@ -1,71 +1,75 @@
 <?php
 
 namespace Mesd\FileDocumentBundle\Controller;
-use Mesd\FileDocumentBundle\Entity\Document;
+
 use Mesd\FileDocumentBundle\FormType\DocumentType;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DocumentController extends Controller
 {
-    public function uploadAction( Request $request ) {
-        $document = $this->get( 'document' );
+    public function uploadAction(Request $request)
+    {
+        $document = $this->get('document');
 
-        $form = $this->createForm( new DocumentType()
+        $form = $this->createForm(new DocumentType()
             , $document
         );
 
-        return $this->render( 'MesdFileDocumentBundle:Document:upload.html.twig'
-            , array(
-                'form' => $form->createView()
-            ) );
+        return $this->render('MesdFileDocumentBundle:Document:upload.html.twig'
+            , [
+                'form' => $form->createView(),
+            ]);
     }
 
-    public function receiveUploadAction( Request $request ) {
-        $document = $this->get( 'document' );
+    public function receiveUploadAction(Request $request)
+    {
+        $document = $this->get('document');
 
-        $form = $this->createForm( new DocumentType()
+        $form = $this->createForm(new DocumentType()
             , $document
         );
 
-        $form->bindRequest( $request );
+        $form->bind($request);
 
-        if ( $form->isValid() ) {
-            if ( $document->getFilename() === null ) {
-                $document->setFilename( $document->getFile()->getClientOriginalName() );
+        if ($form->isValid()) {
+            if ($document->getFilename() === null) {
+                $document->setFilename($document->getFile()->getClientOriginalName());
             }
 
             $em = $this->getDoctrine()->getManager();
-            $em->persist( $document );
+            $em->persist($document);
             $em->flush();
 
-            return $this->redirect( $this->generateUrl( 'default' ) );
+            return $this->redirect($this->generateUrl('default'));
         }
 
-        return $this->render( 'MesdFileDocument_upload'
-            , array(
-                'form' => $form->createView()
-            ) );
+        return $this->render('MesdFileDocument_upload'
+            , [
+                'form' => $form->createView(),
+            ]);
     }
 
-    public function downloadAction( Request $request, $id ) {
+    public function downloadAction(
+        Request $request,
+                $id
+    ) {
         $em = $this->getDoctrine()->getManager();
 
-        $document = $em->getRepository( 'MesdFileDocumentBundle:Document' )->find( $id );
+        $document = $em->getRepository('MesdFileDocumentBundle:Document')->find($id);
 
-        if ( !$document ) {
-            throw $this->createNotFoundException( 'Unable to find the document' );
+        if (!$document) {
+            throw $this->createNotFoundException('Unable to find the document');
         }
 
-        $headers = array(
-            'Content-Type' => $document->getMimeType()?:'file',
-            'Content-Disposition' => 'attachment; filename="'.$document->getFilename().'"'
-        );
+        $headers = [
+            'Content-Type'        => $document->getMimeType() ?: 'file',
+            'Content-Disposition' => 'attachment; filename="' . $document->getFilename() . '"',
+        ];
 
-        $filename = $document->getPath().'/'.$document->getHash();
+        $filename = $document->getPath() . '/' . $document->getHash();
 
-        return new Response( file_get_contents( $filename ), 200, $headers );
+        return new Response(file_get_contents($filename), 200, $headers);
     }
 }
